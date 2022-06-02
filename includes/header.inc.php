ab355d07bad88a78780d3e3e035fed772e3b0f2c
MAbdallah11 <abdallahmac9@gmail.com>
2022-06-02 21:57:02 +0300

    Added a navigation bar.

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,5 +9,28 @@
     <title>Document</title>
 </head>
 <body>
-    
-    
+
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" href="src/admin/index.php">Administrator</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" href="#">Doctor</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" href="#">Patient</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+        
