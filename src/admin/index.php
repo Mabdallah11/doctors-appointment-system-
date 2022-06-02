9243c586a4fb1368ba88819a08a1b2065029e974
MAbdallah11 <abdallahmac9@gmail.com>
2022-06-02 23:43:04 +0300

    Added a login form.

--- src/admin/index.php
+++ src/admin/index.php
@@ -4,7 +4,16 @@ include_once "includes/header.inc.php";
 
 <div class="container">
     <h1>Administrator</h1>
-
+    <form>
+        <div class="mb-3">
+            <label class="form-label">Username</label>
+            <input name="admin-username" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input name="admin-password" type="password" class="form-control" id="exampleInputPassword1">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
 
 
