79ab524a1e436ecc323ba712b62fdd199a20c690
MAbdallah11 <abdallahmac9@gmail.com>
2022-06-02 21:55:05 +0300

    Header included

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "includes/header.inc.php";
+?>
+
+    <div class="container">
+        <h1>Doctors Appointment System</h1>
+    </div>
+
+<?php
+    include_once "includes/footer.inc.php";
+?>
