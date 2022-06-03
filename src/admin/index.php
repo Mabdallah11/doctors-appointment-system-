006c40a9a0f8febf209f480e9e82556591f03f67
MAbdallah11 <abdallahmac9@gmail.com>
2022-06-03 03:18:48 +0300

    Added an error message section.

--- src/admin/index.php
+++ src/admin/index.php
@@ -4,6 +4,17 @@ include_once "includes/header.inc.php";
 
 <div class="container">
     <h1>Administrator</h1>
+
+    <?php
+        if(isset($_GET['err_msg'])){
+            ?>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $_GET['err_msg']; ?>
+        </div>    
+            <?php
+        }
+    ?>
+    
     <form>
         <div class="mb-3">
             <label class="form-label">Username</label>
@@ -11,7 +22,7 @@ include_once "includes/header.inc.php";
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input name="admin-password" type="password" class="form-control" id="exampleInputPassword1">
+            <input name="admin-password" type="password" class="form-control" id="exampleInputPassword1"><br>
         <button type="submit" name="admin-login" class="btn btn-primary">login</button>
     </form>
 </div>
