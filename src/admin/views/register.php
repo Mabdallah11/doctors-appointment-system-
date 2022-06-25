0066f65669bdd265c4ee81c20f149d171960b394
MAbdallah11 <abdallahmac9@gmail.com>
2022-06-25 11:39:18 +0300

    loginp ages

--- src/admin/views/register.php
+++ src/admin/views/register.php
@@ -1,46 +1,45 @@
-<form class="row g-3">
-  <div class="col-md-6">
-    <form action="../config/process-register.php" method="POST">
-    <label for="inputEmail4" class="form-label">First Name</label><br>
-    <input type="text" class="form-control" id="inputEmail4">
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Last Name</label><br>
-    <input type="text" class="form-control" id="inputPassword4">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Email</label><br>
-    <input type="text" class="form-control" id="inputAddress" placeholder="">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">ID Number</label><br>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="">
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">Phone Number</label><br>
-    <input type="text" class="form-control" id="inputCity">
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">Gender</label><br>
-    <select id="inputState" class="form-select">
-      <option selected>Male</option>
-      <option>Female</option>
-    </select>
-  </div><br>
-  
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Password</label><br>
-    <input type="password" class="form-control" id="inputZip">
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label><br>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
+<div class="col-md-6">
+  <form action="../config/process-register.php" method="POST">
+        <label for="inputEmail4" class="form-label">First Name</label><br>
+        <input type="text" class="form-control" id="inputEmail4">
+      </div>
+      <div class="col-md-6">
+        <label for="inputPassword4" class="form-label">Last Name</label><br>
+        <input type="text" class="form-control" id="inputPassword4">
+      </div>
+      <div class="col-12">
+        <label for="inputAddress" class="form-label">Email</label><br>
+        <input type="text" class="form-control" id="inputAddress" placeholder="">
+      </div>
+      <div class="col-12">
+        <label for="inputAddress2" class="form-label">ID Number</label><br>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="">
+      </div>
+      <div class="col-md-6">
+        <label for="inputCity" class="form-label">Phone Number</label><br>
+        <input type="text" class="form-control" id="inputCity">
+      </div>
+      <div class="col-md-4">
+        <label for="inputState" class="form-label">Gender</label><br>
+        <select id="inputState" class="form-select">
+          <option selected>Male</option>
+          <option>Female</option>
+        </select>
+      </div><br>
+      
+      <div class="col-md-2">
+        <label for="inputZip" class="form-label">Password</label><br>
+        <input type="password" class="form-control" id="inputZip">
+      </div>
+      <div class="col-12">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Check me out
+          </label><br>
+        </div>
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary">Sign in</button>
+  </form>
+</div>
